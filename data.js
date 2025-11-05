a355a45c7a2aed5f8b6c10b78399b31ca1c1b00a
tubuan03 <tubuan03@gmail.com>
2025-11-06 07:45:11 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+// data.js
+const characters = [
+  {name: "センティネル", level: 1, insight: 0, resonance: 1, statue: 0, owned: false, img: "images/sentinel.png"},
+  {name: "モルダー", level: 1, insight: 0, resonance: 1, statue: 0, owned: false, img: "images/moldir.png"},
+  {name: "ウルリッヒ", level: 1, insight: 0, resonance: 1, statue: 0, owned: false, img: "images/ulrich.png"},
+  {name: "ノーティカ", level: 1, insight: 0, resonance: 1, statue: 0, owned: false, img: "images/nautika.png"},
+  {name: "ドア", level: 1, insight: 0, resonance: 1, statue: 0, owned: false, img: "images/Door.png"}
+];
+
+const mindsets = [
+  {name: "細流に流れ込んで", level: 1, amplify: 0, owned: false, img: "images/.png"},
+  {name: "帰郷の追悼", level: 1, amplify: 0, owned: false, img: "images/.png"},
+  {name: "ふとしたきらめき", level: 1, amplify: 0, owned: false, img: "images/.png"},
+  {name: "喪失", level: 1, amplify: 0, owned: false, img: "images/.png"}
+];
