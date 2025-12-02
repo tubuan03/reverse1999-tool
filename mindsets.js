942fc25346f9c64c1d29c42384ac9ba9d95bd5de
tubuan03 <tubuan03@gmail.com>
2025-12-02 09:46:38 +0900

    Update mindsets.js

--- mindsets.js
+++ mindsets.js
@@ -1,5 +1,12 @@
 const mindsets = [
   {
+    name: "さすらいの即興曲",
+    level: 1,
+    amplify: 0,
+    owned: false,
+    img: "imagessasuraino.png",
+  },
+  {
     name: "種に囁きかけ",
     level: 1,
     amplify: 0,
@@ -644,5 +651,6 @@ const mindsets = [
     img: "images/sousitu.png",
   },
 ];
+
 
 
